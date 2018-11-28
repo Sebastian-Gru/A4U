29f4a519d6e2ceb588fb3e14351ee1311598060d
sgruessi <sgruessi@smail.uni-koeln.de>
2018-11-28 16:53:09 +0100

    Initial commit

---
+++ app/src/androidTest/java/git/comsebastian_grua4u/httpsgithub/a4u/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package git.comsebastian_grua4u.httpsgithub.a4u
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("git.comsebastian_grua4u.httpsgithub.a4u", appContext.packageName)
+    }
+}
